921798e3b7a5ed90e5aac818d4d8dd0dcfea15b1
ckallidonis <kallidonis@gmail.com>
2020-01-07 17:09:34 -0500

    Using namespace quda in mg_mugiq

--- include/mg_mugiq.h
+++ include/mg_mugiq.h
@@ -3,13 +3,15 @@
 
 #include <multigrid.h> //- The QUDA MG header file
 
+using namespace quda;
+
 class MG_Mugiq {
 
   friend class Eigsolve_Mugiq;
   
 private:
   
-  quda::multigrid_solver *mg_solver; // The multigrid structure
+  multigrid_solver *mg_solver; // The multigrid structure
 
   QudaMultigridParam *mgParams; // Multigrid parameters
 
@@ -17,15 +19,15 @@ private:
 
   bool mgInit; // Initialization switch
 
-  quda::Dirac *diracCoarse; // The Coarse Dirac operator
+  Dirac *diracCoarse; // The Coarse Dirac operator
 
-  quda::DiracMatrix *matCoarse; // Wrapper for the Coarse operator
+  DiracMatrix *matCoarse; // Wrapper for the Coarse operator
 
-  quda::TimeProfile &mg_profile; // Profiling
+  TimeProfile &mg_profile; // Profiling
   
 public:
   
-  MG_Mugiq(QudaMultigridParam *mgParams_, QudaEigParam *eigParams_, quda::TimeProfile &profile_); // Constructor
+  MG_Mugiq(QudaMultigridParam *mgParams_, QudaEigParam *eigParams_, TimeProfile &profile_); // Constructor
   
   virtual ~MG_Mugiq(); // Destructor
   
--- lib/mg_mugiq.cpp
+++ lib/mg_mugiq.cpp
@@ -1,7 +1,7 @@
 #include <mg_mugiq.h>
 
 
-MG_Mugiq::MG_Mugiq(QudaMultigridParam *mgParams_, QudaEigParam *eigParams_, quda::TimeProfile &profile_) :
+MG_Mugiq::MG_Mugiq(QudaMultigridParam *mgParams_, QudaEigParam *eigParams_, TimeProfile &profile_) :
   mgParams(mgParams_),
   eigParams(eigParams_),
   mgInit(false),
@@ -9,17 +9,17 @@ MG_Mugiq::MG_Mugiq(QudaMultigridParam *mgParams_, QudaEigParam *eigParams_, quda
   matCoarse(nullptr),
   mg_profile(profile_)
 {
-  mg_solver = new quda::multigrid_solver(*mgParams, mg_profile);
+  mg_solver = new multigrid_solver(*mgParams, mg_profile);
 
   diracCoarse = mg_solver->mg->getDiracCoarse();
-  if(typeid(*diracCoarse) != typeid(quda::DiracCoarse)) errorQuda("The Coarse Dirac operator must not be preconditioned!\n");
+  if(typeid(*diracCoarse) != typeid(DiracCoarse)) errorQuda("The Coarse Dirac operator must not be preconditioned!\n");
 
   // Create the Coarse Dirac operator
   // That's the operator whose eigenvalues will be computed
-  if (!eigParams->use_norm_op && !eigParams->use_dagger)     matCoarse = new quda::DiracM(*diracCoarse);
-  else if (!eigParams->use_norm_op && eigParams->use_dagger) matCoarse = new quda::DiracMdag(*diracCoarse);
-  else if (eigParams->use_norm_op && !eigParams->use_dagger) matCoarse = new quda::DiracMdagM(*diracCoarse);
-  else if (eigParams->use_norm_op && eigParams->use_dagger)  matCoarse = new quda::DiracMMdag(*diracCoarse);
+  if (!eigParams->use_norm_op && !eigParams->use_dagger)     matCoarse = new DiracM(*diracCoarse);
+  else if (!eigParams->use_norm_op && eigParams->use_dagger) matCoarse = new DiracMdag(*diracCoarse);
+  else if (eigParams->use_norm_op && !eigParams->use_dagger) matCoarse = new DiracMdagM(*diracCoarse);
+  else if (eigParams->use_norm_op && eigParams->use_dagger)  matCoarse = new DiracMMdag(*diracCoarse);
   
   mgInit = true;
 }
