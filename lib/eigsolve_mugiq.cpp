bec6eb22f685802869b900ac11c6989cb61fccb1
ckallidonis <kallidonis@gmail.com>
2020-01-07 14:35:05 -0500

    Defined makeChecks() function of Eigsolve_Mugiq class

--- lib/eigsolve_mugiq.cpp
+++ lib/eigsolve_mugiq.cpp
@@ -1,4 +1,5 @@
 #include <eigsolve_mugiq.h>
+#include <util_quda.h>
 
 Eigsolve_Mugiq::Eigsolve_Mugiq(QudaEigParam *eigParams_) :
   eigParams(eigParams_),
@@ -9,5 +10,12 @@ Eigsolve_Mugiq::Eigsolve_Mugiq(QudaEigParam *eigParams_) :
 Eigsolve_Mugiq::~Eigsolve_Mugiq(){
 }
 
-void makeChecks(){
+void Eigsolve_Mugiq::makeChecks(){
+
+  if (invParams->dslash_type != QUDA_WILSON_DSLASH && invParams->dslash_type != QUDA_CLOVER_WILSON_DSLASH)
+    errorQuda("%s: Supports only Wilson and Wilson-Clover operators for now!\n", __func__);
+
+  // No polynomial acceleration on non-symmetric matrices
+  if (eigParams->use_poly_acc && !eigParams->use_norm_op && !(invParams->dslash_type == QUDA_LAPLACE_DSLASH))
+    errorQuda("%s: Polynomial acceleration with non-symmetric matrices not supported", __func__);
 }
