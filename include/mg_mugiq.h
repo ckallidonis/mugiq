89b6b6ee0b56a505b440aed862cd587f21f04dd8
ckallidonis <kallidonis@gmail.com>
2020-01-15 15:42:06 -0500

    Moved constructor of MG_Mugiq struct inside its definition

--- include/mg_mugiq.h
+++ include/mg_mugiq.h
@@ -17,7 +17,19 @@ struct MG_Mugiq {
 
   TimeProfile &profile; // Profiling
   
-  MG_Mugiq(QudaMultigridParam *mgParams_, TimeProfile &profile_); // Constructor
+  MG_Mugiq(QudaMultigridParam *mgParams_, TimeProfile &profile_) :
+    mgParams(mgParams_),
+    mgInit(false),
+    diracCoarse(nullptr),
+    profile(profile_)
+  {
+    mg_solver = new multigrid_solver(*mgParams, profile);
+    
+    diracCoarse = mg_solver->mg->getDiracCoarse();
+    if(typeid(*diracCoarse) != typeid(DiracCoarse)) errorQuda("The Coarse Dirac operator must not be preconditioned!\n");
+
+    mgInit = true;
+  }
   
   virtual ~MG_Mugiq(){
     if (mg_solver) delete mg_solver;
