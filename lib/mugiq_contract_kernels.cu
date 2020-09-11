47e117336db494ce83682e75e8e700fb7dd30bc0
ckallidonis <kallidonis@gmail.com>
2020-09-11 12:54:08 -0400

    Added __shared__ memory buffer; function to cast __constant__ memory gamma-coeffs to their structure type

--- lib/mugiq_contract_kernels.cu
+++ lib/mugiq_contract_kernels.cu
@@ -1,5 +1,19 @@
 #include <mugiq_contract_kernels.cuh>
 
+
+//- Shared memory buffer with the left and right vectors, as well as the contraction data
+template <typename Float>
+extern __shared__ complex<Float> shmemBuf[];
+
+
+//- Function that casts the __constant__ memory variable containing the gamma Coefficients
+//- to its structure type, GammaCoeff
+template <typename Float>
+inline __device__ const GammaCoeff<Float>* gCoeff() {
+  return reinterpret_cast<const GammaCoeff<Float>*>(cGamma);
+}
+
+
 template <typename Float>
 __global__ void loopContract_kernel(complex<Float> *loopData, LoopContractArg<Float> *arg){
 
