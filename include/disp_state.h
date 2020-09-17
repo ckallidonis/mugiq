e6a1f18d95aa12061c10171e54f2008d30b1dd70
ckallidonis <kallidonis@gmail.com>
2020-09-17 15:04:29 -0400

    Some definitions and enums concerning displacemets

--- include/disp_state.h
+++ include/disp_state.h
@@ -18,6 +18,12 @@ private:
   template <typename Float>
   friend class Loop_Mugiq;
 
+  const char *DisplaceFlagArray = "XxYyZzTt" ;
+  const char *DisplaceTypeArray[] = {"Covariant"};
+  
+  const char *DisplaceDirArray[]  = {"x", "y", "z", "t"};
+  const char *DisplaceSignArray[] = {"-", "+"};  
+  
   //- The pointer with the gauge data coming from the interface
   void *gaugePtr[N_DIM_];
 
@@ -53,6 +59,46 @@ public:
 
 
 };
+
+
+//- Some enums about the Displacements
+
+#define nDisplaceFlags 8
+#define nDisplaceTypes 1
+
+typedef enum DisplaceFlag_s {
+  DispStr_None = -1,
+  DispStr_X = 0,  // +x
+  DispStr_x = 1,  // -x
+  DispStr_Y = 2,  // +y
+  DispStr_y = 3,  // -y
+  DispStr_Z = 4,  // +z
+  DispStr_z = 5,  // -z
+  DispStr_T = 6,  // +t
+  DispStr_t = 7,  // -t
+} DisplaceFlag;
+
+
+typedef enum DisplaceDir_s {
+  DispDirNone = -1,
+  DispDir_x = 0,
+  DispDir_y = 1,
+  DispDir_z = 2,
+  DispDir_t = 3
+} DisplaceDir;
+
+
+typedef enum DisplaceSign_s {
+  DispSignNone  = -1,
+  DispSignMinus =  0,
+  DispSignPlus  =  1
+} DisplaceSign;
+
+
+typedef enum DisplaceType_s {
+  InvalidDisplace = -1,
+  CovDisplace = 0
+} DisplaceType;
 
 
 #endif
